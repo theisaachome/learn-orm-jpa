fb36bab33dfbab109940ddbfca381a7b72d4663b
theisaachome <65864160+theisaachome@users.noreply.github.com>
2024-10-16 22:31:18 +0800

    clean directory.

--- learn-spring-data-jpa/src/main/java/com/highway/sms/learnspringdatajpa/repo/UserRepo.java
+++
@@ -1,7 +0,0 @@
-package com.highway.sms.learnspringdatajpa.repo;
-
-import com.highway.sms.learnspringdatajpa.oneToOne.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Integer> {
-}
